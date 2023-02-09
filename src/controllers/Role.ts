879edef21473e03cb640a7aa5355bf2d894498bd
Wil Macedo <wil.macedo.sa@gmail.com>
2023-02-09 13:55:05 -0300

    feat: add error catching on role controller

--- src/controllers/Role.ts
+++ src/controllers/Role.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@prisma/client';
 import { Request, Response } from 'express';
 import { prisma } from '../database/client';
 import { ErrorType } from '../types';
@@ -10,25 +11,31 @@ export class RoleController {
       return;
     }
 
-    const { name } = request.body;
+    try {
+      const role = await prisma.role.create({ data: request.body });
 
-    const role = await prisma.role.create({
-      data: {
-        name,
-      },
-    });
-
-    response.role.show(role);
+      response.role.show(role);
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        response.role.error(e);
+      }
+    }
   }
 
   async findMany(_: Request, response: Response) {
-    const roles = await prisma.role.findMany();
-    if (roles.length === 0) {
-      response.role.error({ type: ErrorType.EMPTY });
-      return;
-    }
+    try {
+      const roles = await prisma.role.findMany();
+      if (roles.length === 0) {
+        response.role.error({ type: ErrorType.EMPTY });
+        return;
+      }
 
-    response.role.many(roles);
+      response.role.many(roles);
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        response.role.error(e);
+      }
+    }
   }
 
   async findOne(request: Request, response: Response) {
@@ -39,13 +46,19 @@ export class RoleController {
     }
 
     const idNum = Number(id);
-    const role = await prisma.role.findUnique({ where: { id: idNum } });
-    if (!role) {
-      response.role.error({ type: ErrorType.NOT_FOUND });
-      return;
-    }
+    try {
+      const role = await prisma.role.findUnique({ where: { id: idNum } });
+      if (!role) {
+        response.role.error({ type: ErrorType.NOT_FOUND });
+        return;
+      }
 
-    response.role.show(role);
+      response.role.show(role);
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        response.role.error(e);
+      }
+    }
   }
 
   async update(request: Request, response: Response) {
@@ -56,12 +69,18 @@ export class RoleController {
     }
 
     const idNum = Number(id);
-    const updateRole = await prisma.role.update({
-      where: { id: idNum },
-      data: request.body,
-    });
+    try {
+      const updateRole = await prisma.role.update({
+        where: { id: idNum },
+        data: request.body,
+      });
 
-    response.role.show(updateRole);
+      response.role.show(updateRole);
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        response.role.error(e);
+      }
+    }
   }
 
   async delete(request: Request, response: Response) {
@@ -72,8 +91,14 @@ export class RoleController {
     }
 
     const idNum = Number(id);
-    await prisma.role.delete({ where: { id: idNum } });
+    try {
+      await prisma.role.delete({ where: { id: idNum } });
 
-    response.status(204).json();
+      response.status(204).json();
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        response.role.error(e);
+      }
+    }
   }
 }
